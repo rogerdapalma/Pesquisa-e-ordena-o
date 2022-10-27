10a71efcdbd459d8c93624296ee514853c48275d
Roger <94903084+rogerdapalma@users.noreply.github.com>
2022-10-26 21:15:30 -0300

    Add files via upload

---
+++ METADOS DE ORDENAÇÃO atualizado/Program.cs
@@ -0,0 +1,11 @@
+namespace metodosOrdenacao {
+    internal static class Program {
+       
+        [STAThread]
+        static void Main() {
+           
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Frame());
+        }
+    }
+}
